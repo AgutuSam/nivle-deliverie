1274f25f76a6ef8bd2811d1b5d4535892fd18a41
AgutuSam <sammyhardcell@gmail.com>
2021-02-21 16:27:51 +0300

    Updates

---
+++ precache-manifest.94c364308721d8f8e09e7eef57e7ee44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e95e9cdf367cdcb93fb",
+    "url": "/static/css/main.aa4af7b3.chunk.css"
+  },
+  {
+    "revision": "3e95e9cdf367cdcb93fb",
+    "url": "/static/js/main.3e95e9cd.chunk.js"
+  },
+  {
+    "revision": "1eb3b27dcddaebbb927c",
+    "url": "/static/js/1.1eb3b27d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc4bb86fe70678f4116856b4c2bbd659",
+    "url": "/index.html"
+  }
+];
